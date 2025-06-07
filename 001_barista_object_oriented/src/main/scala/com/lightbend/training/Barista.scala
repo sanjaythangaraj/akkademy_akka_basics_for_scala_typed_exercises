6faede80884f148d640e84e2bdb2d092cf0f1dda
sanjaythangaraj <sanjaythangaraj@outlook.in>
2025-06-07 18:33:54 +0530

    exercise 1 sol

--- 001_barista_object_oriented/src/main/scala/com/lightbend/training/Barista.scala
+++ 001_barista_object_oriented/src/main/scala/com/lightbend/training/Barista.scala
@@ -2,16 +2,32 @@ package com.lightbend.training
 
 import scala.collection._
 import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 
 object Barista {
 
   final case class OrderCoffee(whom: String, coffee: Coffee)
 
-  def apply(): Behavior[OrderCoffee] = ???
+  def apply(): Behavior[OrderCoffee] =
+    Behaviors.setup(context => new BaristaBehavior(context))
 
   def printOrders(orders: Set[(String, Coffee)]): String = {
-      val formattedOrders = orders.map(order => s"${order._1}->${order._2}")
-        .reduce((acc, s) => acc + "," + s)
-      s"[$formattedOrders]"
+    val formattedOrders = orders
+      .map(order => s"${order._1}->${order._2}")
+      .reduce((acc, s) => acc + "," + s)
+    s"[$formattedOrders]"
+  }
+
+  class BaristaBehavior(context: ActorContext[OrderCoffee])
+      extends AbstractBehavior[OrderCoffee](context) {
+
+    private val orders: mutable.Map[String, Coffee] = mutable.Map.empty
+
+    override def onMessage(msg: OrderCoffee): Behavior[OrderCoffee] = msg match {
+      case OrderCoffee(whom, coffee) =>
+        orders.put(whom, coffee)
+        context.log.info("Orders:" + printOrders(orders.toSet))
+        this
+    }
   }
 }
